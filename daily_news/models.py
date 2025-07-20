5bf4c5d555c826b4b6675e85a27812ea5e508f00
colo993 <wakula993@gmail.com>
2025-07-20 11:16:19 +0200

    work in progress

--- daily_news/models.py
+++ daily_news/models.py
@@ -1,9 +1,42 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Keyword(models.Model):
+	word = models.CharField(max_length=50, unique=True, null=False, blank=False)
+
+	def __str__(self):  # pyright: ignore [reportIncompatibleMethodOverride]
+		return self.word
 
 
 class Topic(models.Model):
-	name = models.CharField(max_length=20)
-	target_words = models.CharField(max_length=100)
-	amount_of_news = models.IntegerField(default=1) # pyright: ignore [reportArgumentType]
-	created_at = models.DateTimeField('creation date')
-	topic_description = models.CharField(max_length=200)
+	name = models.CharField(max_length=50)
+	max_news_per_day = models.IntegerField(
+		default=0,  # pyright: ignore [reportArgumentType]
+		help_text='Maximum number of news articles to reveive daily for this topic.',
+		# TODO: add validation of max news
+	)
+
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
+	keywords = models.ManyToManyField(Keyword, related_name='topics_using_this_keyword')
+	created_at = models.DateTimeField(verbose_name='creation date', auto_now_add=True)
+
+	description = models.TextField(
+		blank=True,
+		null=True,
+	)
+
+	class Meta:
+		unique_together = ('user', 'name')
+		verbose_name = 'User Topic'
+		verbose_name_plural = 'User Topics'
+		ordering = ['user__username', '-created_at']
+
+	def __str__(self):  # pyright: ignore [reportIncompatibleMethodOverride]
+		return self.name
+
+	def was_created_recently(self):
+		return self.created_at >= timezone.now() - datetime.timedelta(days=1)
--- daily_news/views.py
+++ daily_news/views.py
@@ -1,5 +1,34 @@
 from django.http import HttpResponse
+from .models import Topic, Keyword
+from django.shortcuts import render, get_object_or_404, redirect
 
 
 def index(request):
-	return HttpResponse('Hello, world. You are at the daily_news index')
+	latest_topic_list = Topic.objects.order_by('-created_at')[:5]
+	context = {'latest_topic_list': latest_topic_list}
+	return render(request, 'daily_news/index.html', context)
+
+
+def detail(request, topic_id):
+	topic = get_object_or_404(Topic, pk=topic_id)
+	return render(request, 'daily_news/detail.html', {'topic': topic})
+
+
+def keywords(request, topic_id):
+	topic = get_object_or_404(Topic, pk=topic_id)
+	try:
+		inserted_keyword = request.POST.get('my_keyword')
+		keyword = Keyword(word=inserted_keyword)
+		keyword.save()
+		topic.keywords.add(keyword)
+	except:
+		return render(
+			request,
+			'daily_news/detail.html',
+			{'topic': topic, 'error_message': "You didn't provide any keyword."},
+		)
+	return redirect('daily_news:detail', topic_id=topic.id)
+
+
+def description(request, topic_id):
+	return HttpResponse(f"You're looking at descrition of topic {topic_id}")
